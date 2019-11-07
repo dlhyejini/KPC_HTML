aa8b14faac0488bb1137bd9a1c7568976b6c8c11
dlhyejini <dlhyejini@gmail.com>
2019-11-07 15:36:51 +0900

    공지사항및 자료실 마크업과 디자인 및 인터렉션

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,11 +1,16 @@
 var menu_item = $('.menu > li');
 var btn_menu = $('.menu [role="button"]');
 var sub_link = $('.sub-menu a');
+var tab_act= $('.board section');
+var tab_section =$('.tab a');
+var tab_list_link =$('.board li a');
 
 menu_item.addClass('menu-item');
 btn_menu.addClass('btn-menu');
 sub_link.addClass('icon-dot-circled');
+tab_list_link.addClass('icon-dot-circled')
 
+//메인메뉴
 btn_menu.click(function(e){
   var current_class = $(this).parent().toggleClass('menu-act');
    e.preventDefault();   
@@ -16,4 +21,10 @@ btn_menu.click(function(e){
    } else{
    $(this).parent().addClass('menu-act');
    }
+});
+//탭 메뉴
+tab.click(function(e){
+  e.preventDefault();
+  tab_section.removeClass('tab-act');
+  $(this).parent().parent() .addClass('tab-act');
 });
